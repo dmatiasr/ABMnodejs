8f63c6011cb30a5bbe890569939ae1e0a5ceffb9
dmatiasr <matu_dmr@hotmail.com>
2016-11-23 14:36:01 -0300

    Dao create with route

--- ABMExampleNodeJs/dao/userdao.js
+++ ABMExampleNodeJs/dao/userdao.js
@@ -1,61 +1,91 @@
 	var userModel= require('../models/user');
 
-	//GET
-	exports.findAllU= function (req, res) {
-		userModel.find( function (err,us) {
-			if (!err) res.send(us);
-			else console.log ('ERROR findAll '+err);
-		});
-	}
-	//
-	exports.findByIDU= function(req, res){
-		userModel.findByID(function (err,us) {
-			if (!err) res.send(us);
-			else console.log ('ERROR findByID '+err);
-		});
-	}
-
 	//POST 
-	exports.addU= function (req, res) {
-		//console.log('POST '+req.body);
+	exports.addU= function (pname,pemail, callback){ 
 	 	
 	 	var addUserModel = new userModel({
-	 		name : req.body.name,
-	 		email: req.body.email
+	 		name : pname,
+	 		email: pemail
 	 	});
-	 	
+
 	 	addUserModel.save(function (err) {
 	 		if (!err){
 	 			console.log('usuario guardado');
-	 			return true;
+	 			ok='ok';
+	 			callback(err,ok);
 	 		} 
 	 		else{
-	 			console.log ('ERROR add '+e);
-	 			return false;	
+				console.log('usuario NO guardado'+err);	 			
+	 			ok='no';
+				callback(err,ok);	
 	 		} 
-	 	});
+	 	});	 
+	}	
+
+
+
+	//{
+		//console.log('POST '+req.body);
+	 	
+	 //	var addUserModel = new userModel({
+	 //		name : pname,
+	 //		email: pemail
+	 //	});
+	 	
+	// 	addUserModel.save(function (err) {
+	 //		if (!err){
+	 	//		console.log('usuario guardado');
+	 	//		return true;
+	 	//	} 
+	 	//	else{
+	 	//		console.log ('ERROR add '+e);
+	 	//		return false;	
+	 	//	} 
+	// 	});
 		
 		//res.send(addUserModel);
+	//}
+
+	//GET
+	exports.findAllU= function (req, res) {
+		userModel.find( {},function (err,us) {
+			if (!err){ 
+				return (us);
+			}else {
+				console.log ('ERROR findAll '+err);
+				return null;
+			}
+				
+		});
 	}
+	//
+	//exports.findByIDU= function(req, res){
+	//	userModel.findByID(function (err,us) {
+	//		if (!err) res.send(us);
+	//		else console.log ('ERROR findByID '+err);
+	//	});
+	//}
+
+	
 
 	//PUT UPDATE
-	exports.updateU= function (req, res) {
-		userModel.findByID(req.params.id, function (err,user) {
-			user.name = user.body.name;
-			user.email= user.body.email;
+	//exports.updateU= function (req, res) {
+	//	userModel.findByID(req.params.id, function (err,user) {
+	//		user.name = user.body.name;
+	//		user.email= user.body.email;
 			
-		});
-		userModel.save(function (err) {
-			if (!err) console.log ('Usuario actualizado');
-			else console.log ('ERROR update '+err);
-		});
-	}
+	//	});
+	//	userModel.save(function (err) {
+	//		if (!err) console.log ('Usuario actualizado');
+	//		else console.log ('ERROR update '+err);
+	//	});
+	//}
 	//DELETE
-	exports.removeU = function (req, res) {
-		userModel.findByID(req.params.id, function (err,user) {
-			user.remove(function (err) {
-				if (err) console.log ('No se ha podido eliminar '+err);
-				else console.log ('Eliminado');
-			});
-		});			
-	}
+	//exports.removeU = function (req, res) {
+	//	userModel.findByID(req.params.id, function (err,user) {
+	//		user.remove(function (err) {
+	//			if (err) console.log ('No se ha podido eliminar '+err);
+	//			else console.log ('Eliminado');
+	//		});
+	//	});			
+	//}
--- ABMExampleNodeJs/routes/index.js
+++ ABMExampleNodeJs/routes/index.js
@@ -5,17 +5,30 @@ var userdao = require('../dao/userdao');
 module.exports = function (app) {
 
 	app.get('/',function (req, res) {
-			res.render('index', { title: 'Lista de usuarios' });
-
+	//	var query= userdao.findAllU(req,res);
+	//	if (query==null) res.render('index', { title: 'Lista de usuarios' });
+	//	else res.render('index', { title: 'Lista de usuarios', allusers: query });
+		res.render('index', { title: 'Lista de usuarios' });		
 	})
 	
 	app.post('/',function (req, res) {
 		
 		console.log('Pase por aqui '+ req.name);
-	 	var result=userdao.addU(req,res);
-	 	if (result) res.render('index', { title: 'Lista de usuarios' });
-	 	else res.render('index', { title: 'Lista de usuarios' });
-					
-	
+	 	
+	 	userdao.addU(req.params.name, req.params.email, function (e, data) {
+	 		if (e) console.log('error de creacion '+e);
+	 		if (data=='ok'){
+	 			res.status(200).render('index', { title: 'Lista de usuarios' });
+	 		} 			
+	 		else {
+	 			res.status(500).render('index', { title: 'Lista de usuarios' });	
+	 		} 
+	 	});
+		
 	})
+//	app.get('/allusers', function (req, res) {
+//		var query = userdao.findAllU(req,res);
+//		if (query==null) res.render('index', { title: 'Lista de usuarios' });
+//		else res.render('index', { title: 'Lista de usuarios' },query);
+//	})
 }
